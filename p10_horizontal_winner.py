3bb9b3bced6dc87a81684490ae43ffd195aed0a5
minbaekim <minbae.kim@milliman.com>
2020-03-23 21:13:51 +0900

    horizontal winner

--- p10_horizontal_winner.py
+++ p10_horizontal_winner.py
@@ -6,16 +6,10 @@ game = [[1, 1, 1],
 def win(current_game):
     for row in game:
         print(row)
-
-        all_match = True
         
-        for item in row:
-            
-            if item != row[0]:
-                all_match = False
-
-        if all_match:
-            print('winner!!!')
+        # row[0]이 row의 [다른항목]과 같을경우 'winner', #0일 경우는 제외
+        if row.count(row[0]) == len(row) and row[0] != 0:
+            print('winner')
 
 
 win(game)
